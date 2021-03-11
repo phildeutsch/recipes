d39899c20e27c3d84b3d7f473bb156d713eaf49d
phildeutsch <phil.deutsch@protonmail.com>
2021-03-11 14:53:53 +0000

    Language setting

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,14 @@ from django.db import models
 from django.db.models.signals import post_save 
 from django.dispatch import receiver
 from app.helpers import RandomFileName 
+from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     pass
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    language = models.CharField(max_length=2, default='en', choices=(('en', _('English')), ('de', _('German'))))
     display_name = models.CharField(max_length=128, null=True, blank=True)
     picture = models.ImageField(null=True, blank=True, upload_to=RandomFileName('profiles'))
 
