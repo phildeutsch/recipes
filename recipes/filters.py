60b24e4e7f8a7718caeefdcebbc5c43cae875363
Phil Deutsch <phil.deutsch@protonmail.com>
2021-03-21 18:22:47 +0000

    recipe filters

--- recipes/filters.py
+++ recipes/filters.py
@@ -1,5 +1,5 @@
 import django_filters
-from .models import Dish
+from .models import Dish, Recipe
 from django.utils.translation import ugettext_lazy as _
 
 class DishFilter(django_filters.FilterSet):
@@ -9,3 +9,8 @@ class DishFilter(django_filters.FilterSet):
 
 class RecipeFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
+    course = django_filters.ChoiceFilter(choices=Recipe.COURSE_CHOICES, label=_("Course"))
+    cuisine = django_filters.ChoiceFilter(choices=Recipe.CUISINE_CHOICES, label=_("Cuisine"))
+    vegetarian = django_filters.BooleanFilter()
+    meat = django_filters.BooleanFilter()
+    fish = django_filters.BooleanFilter()
